33ea11bd5d5bb6a187f574b7b3a075da8bb60e85
shilin <1058348744@qq.com>
2024-05-18 17:23:53 +0800

    Added Character & Setup Input
    - 添加模型和动画资源
    - 角色增加前后和转向

--- Source/ActionRougelike/Private/SLCharacter.cpp
+++ Source/ActionRougelike/Private/SLCharacter.cpp
@@ -2,6 +2,7 @@
 
 
 #include "SLCharacter.h"
+// 为了编译更快，在.h中声明弹簧臂和相机的class，在.cpp引用相应头文件
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 
@@ -11,9 +12,16 @@ ASLCharacter::ASLCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// 创建相应实例 <创建的class>("UE中显示的该控件的名字")
+	//<> 表示引入c++自带的文件
+	//"" 表示引入自定义的文件
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
+	// 将相应对象attach，RootComponent是根控件，即角色的胶囊体组件
 	SpringArmComp->SetupAttachment(RootComponent);
 
+	// 创建相应实例 <创建的class>("UE中显示的该控件的名字")
+	// -> 是指向内存中引用的指针
+	// ::是用于访问静态方法或者成员
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
 	CameraComp->SetupAttachment(SpringArmComp);
 }
@@ -24,6 +32,12 @@ void ASLCharacter::BeginPlay()
 	Super::BeginPlay();
 	
 }
+
+void ASLCharacter::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
 
 // Called every frame
 void ASLCharacter::Tick(float DeltaTime)
@@ -37,5 +51,8 @@ void ASLCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &ASLCharacter::MoveForward);
+
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput); //Yaw控制转向
 }
 
